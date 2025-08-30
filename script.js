6f3e56ff2c4bd407c2cc2f6245c527882406135c
TawsifHossain007 <tawsifhossain18.o@gmail.com>
2025-08-30 23:12:19 +0600

    Added everything without responsiveness

--- script.js
+++ script.js
@@ -30,3 +30,67 @@ copy.forEach(function (copyBtn) {
     alert(number + " copied!");
   });
 });
+
+
+// Call Function
+
+const call=document.querySelectorAll(".call-button");
+let callDisplay=document.getElementById("call-show")
+
+let callCount=parseInt(callDisplay.innerText)
+
+call.forEach(function(callBtn){
+  callBtn.addEventListener("click",function(){
+    if(callCount>=20)
+    {
+      callCount -= 20
+      callDisplay.innerText = callCount
+    }
+    else
+    {
+      alert("Not Enough Balance")
+      return;
+    }
+
+    const serviceElement= callBtn.closest(".bg-white").querySelector(".service")
+    const service=serviceElement.innerText
+    const numberElement = callBtn.closest(".bg-white").querySelector(".number")
+    const number = numberElement.innerText;
+
+    alert("📞 Calling " + service + " " + number + "...")
+
+
+    // Call History
+
+    const callHistory=document.getElementById("history-container")
+    const time= new Date().toLocaleTimeString()
+
+
+    const div = document.createElement("div")
+        div.innerHTML = `
+          <div class="bg-white mt-4">
+              <div class="bg-[#FAFAFA] p-3 flex items-center justify-between rounded-2xl">
+                <div class="flex flex-col">
+                  <h1 class="font-bold">${service}</h1>
+                  <p class="text-[#5C5C5C] mt-1">${number}</p>
+                </div>
+                <div>
+                  <p>${time}</p>
+                </div>
+              </div>
+            </div>
+        `;
+
+        callHistory.append(div);
+  })
+})
+
+// Call history clear function
+
+const clear=document.getElementById("clear-button")
+const callHistory=document.getElementById("history-container")
+
+clear.addEventListener("click",function(){
+
+  callHistory.innerHTML="";
+})
